4dc12aabda40620fb9a0cfac6e906234127b8037
leakespeake <barryleake@hotmail.com>
2020-05-04 09:17:14 +0100

    shell script update2

--- modules/aws/stage/services/webserver-demo/vms/bootstrap.sh
+++ modules/aws/stage/services/webserver-demo/vms/bootstrap.sh
@@ -1,7 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
-# startup script for Ubuntu instances - to update list of available packages and their versions - then upgrade installed packages to those versions
-# then install and run a simple web service for testing purposes only
+# startup script for Ubuntu instances
+# to update list of available packages and their versions then upgrade installed packages to those versions
+# also install and run a simple web service for testing purposes only
 
 # base applications such as Docker CE and Python 3 should be baked into the Packer template
 # additional configuration to be done by Ansible to automate tasks specific to a nodes intended purpose
@@ -11,4 +12,4 @@ sleep 30
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y php libapache2-mod-php
 sudo echo "Hello World" > index.html
-sudo nohup busybox httpd -f -p 8080 &
+nohup busybox httpd -f -p 8080 &
