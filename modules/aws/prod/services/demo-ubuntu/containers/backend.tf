2547d04c51170e19ab220d508d56e90826f0bd54
leakespeake <barryleake@hotmail.com>
2020-07-27 07:18:44 +0100

    bb exporter

---
+++ modules/aws/prod/services/demo-ubuntu/containers/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+    backend "s3" {
+        bucket          = "leakespeake-terraform-state-files"
+        key             = "global/s3-bucket/aws/prod/services/demo-ubuntu/containers/terraform.tfstate"
+        region          = "eu-west-2"
+
+        dynamodb_table  = "leakespeake-terraform-state-lock"
+        encrypt         = true
+    }
+}
