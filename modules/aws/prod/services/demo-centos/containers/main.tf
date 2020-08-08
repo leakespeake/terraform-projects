e1e56a11f0cde5123109f14c86b96a061417ef2b
leakespeake <barryleake@hotmail.com>
2020-08-08 17:05:00 +0100

    bb exporter

---
+++ modules/aws/prod/services/demo-centos/containers/main.tf
@@ -0,0 +1,6 @@
+module "docker-bb-exporter" {
+    source = "git@github.com:leakespeake/terraform-reusable-modules.git//docker-containers/blackbox-exporter?ref=0f00b61"
+    docker_host       = "centos-packer-apache01.leakespeake.com"
+    container_name    = "blackbox"
+    container_version = "blackbox-exporter-0.17.0"
+}
