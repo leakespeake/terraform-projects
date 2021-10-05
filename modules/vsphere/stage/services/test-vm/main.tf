34d87013b30f7f1addefee1eb60b5c5702edec72
leakespeake <barryleake@hotmail.com>
2021-10-05 17:09:21 +0100

    vsphere parent module

---
+++ modules/vsphere/stage/services/test-vm/main.tf
@@ -0,0 +1,14 @@
+# parent module for temporary vm to test ansible playbooks
+module "ansible-test" {
+  source = "git@github.com:leakespeake/terraform-reusable-modules.git//vsphere/template-cloning/linux/ubuntu-server-20-04?ref=26decc2"
+
+  vcenter_password  = var.vcenter_password
+  
+  vmname            = "ansible-test"
+
+  network           = "10.2.2.0/24_stg"
+  
+  ipv4              = ["10.2.2.80", "10.2.2.81"]
+  
+  ipv4_gateway      = "10.2.2.1"
+}
