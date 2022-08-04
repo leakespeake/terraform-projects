55ecf58ade70c28bf5fd991fa7caf3cdaa72c77e
leakespeake <barryleake@hotmail.com>
2022-08-04 08:12:58 +0100

    ram increase for clamav

--- modules/vsphere/prod/services/acme/main.tf
+++ modules/vsphere/prod/services/acme/main.tf
@@ -3,7 +3,7 @@ module "acme-prd" {
   source           = "git@github.com:leakespeake/terraform-reusable-modules.git//vsphere/template-cloning/linux/ubuntu-server-22-04?ref=ab11bc0"
   vcenter_password = var.vcenter_password
   vmname           = "acme-prd"
-  memory           = 1024
+  memory           = 2048
   cpus             = 1
   disk1_size       = 15
   network          = "10.1.1.0/24_prd"
