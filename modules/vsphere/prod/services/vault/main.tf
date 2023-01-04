8e977646ef66dd481653ac37ed4be0f7471fd44a
leakespeake <barryleake@hotmail.com>
2023-01-04 15:15:29 +0000

    added cadvisor container

--- modules/vsphere/prod/services/vault/main.tf
+++ modules/vsphere/prod/services/vault/main.tf
@@ -11,3 +11,13 @@ module "vault-prd" {
   ipv4_gateway     = "10.1.1.1"
   template         = "ubuntu-server-22-04-20220602T140217Z"
 }
+
+# DOCKER CONTAINER MODULE
+# sudo ufw allow from {PROMETHEUS_IP} to any port 8080 proto tcp
+# cannot add 'depends_on' to docker provider so comment out until VM deployed or use 'tf plan -target=module.{module-name}'
+module "docker-cadvisor" {
+    source = "git@github.com:leakespeake/terraform-reusable-modules.git//docker-containers/cadvisor?ref=3556bef"
+    docker_host       = "vault-prd-01.int.leakespeake.com"
+    container_name    = "cadvisor"
+    container_version = "v0.46.0"
+}
